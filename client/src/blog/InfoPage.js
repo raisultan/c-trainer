5b34cdba83d55955abef48444e275db303405fab
Baiaman Kasymbaev <baiaman.kasymbaev@gmail.com>
2020-05-06 09:29:54 +0700

    Fix url

--- client/src/blog/InfoPage.js
+++ client/src/blog/InfoPage.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Card } from 'antd';
+import {Card, PageHeader} from 'antd';
 
 export default ({ image, title, text }) => (
   <div>
+    <PageHeader
+      className="site-page-header"
+      title={`/Главная/${title}`}
+      subTitle="Список компрессоров"
+    />
     <Card
       // style={{ width: 240 }}
       cover={<img alt={image} src={`localhost:8000/api/media/${image}`} />}
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // TODO use env
-const baseUrl = 'localhost:8000/api';
+const baseUrl = 'http://localhost:8000/api';
 
 export default {
-  getCompressorList: () => axios.get(`${baseUrl}/catalog`),
+  getCompressorList: () => axios.get(`${baseUrl}/catalog/`),
   login: (username, password) => axios.post(`${baseUrl}/auth/login`, { username, password }),
 }
